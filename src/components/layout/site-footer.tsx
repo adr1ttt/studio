86a0febfc8e9a8d6586fcb8af1a944615ac49b35
042_Adrit Bhatta <adrit.bhatta.csbs28@heritageit.edu.in>
2025-11-13 17:56:15 +0000

    delete the whole logo thing, and rewrite it with this link: https://uplo

--- src/components/layout/site-footer.tsx
+++ src/components/layout/site-footer.tsx
@@ -9,7 +9,7 @@ const socialLinks = [
 ];
 
 const FcBarcelonaLogo = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
+    <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 100 100"
     {...props}
